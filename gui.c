b08a2846e62d44f750294f9721d8dba69b722e33
ArcTLK <adityanair313@gmail.com>
2019-02-18 22:27:54 +0530

    Complete GUI for search

--- gui.c
+++ gui.c
@@ -1,6 +1,14 @@
 #include <windows.h>
 #include "resource.h"
 
+BOOL CALLBACK DlgMain(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK DlgCreation(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK DlgSearch(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+    return DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgMain);
+}
+
 BOOL CALLBACK DlgMain(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     switch(uMsg) {
     case WM_INITDIALOG: {}
@@ -10,7 +18,14 @@ BOOL CALLBACK DlgMain(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     }
     break;
     case WM_COMMAND: {
-        switch(LOWORD(wParam)) {}
+        switch(LOWORD(wParam)) {
+        case IDCREATE:
+            DialogBox(NULL, MAKEINTRESOURCE(IDD_DIALOG2), hwndDlg, (DLGPROC)DlgCreation);
+            break;
+        case IDSEARCH:
+            DialogBox(NULL, MAKEINTRESOURCE(IDD_DIALOG3), hwndDlg, (DLGPROC)DlgSearch);
+            break;
+        }
     }
     break;
     default:
@@ -19,6 +34,82 @@ BOOL CALLBACK DlgMain(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     return TRUE;
 }
 
-int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
-    return DialogBox(hInstance, MAKEINTRESOURCE(DLG_MAIN), NULL, (DLGPROC)DlgMain);
+BOOL CALLBACK DlgCreation(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch(uMsg) {
+    case WM_INITDIALOG: {}
+    break;
+    case WM_CLOSE: {
+        EndDialog(hwndDlg, 0);
+    }
+    break;
+    case WM_COMMAND: {
+        switch(LOWORD(wParam)) {
+        case IDOK: {
+            int nameLen = GetWindowTextLength(GetDlgItem(hwndDlg, IDC_NAME));
+            int numLen = GetWindowTextLength(GetDlgItem(hwndDlg, IDC_NUMBER));
+            if (nameLen > 0 && numLen > 0) {
+                char *name;
+                char *number;
+                name = (char*)GlobalAlloc(GPTR, nameLen + 1);
+                number = (char*)GlobalAlloc(GPTR, numLen + 1);
+                GetDlgItemText(hwndDlg, IDC_NAME, name, nameLen + 1);
+                GetDlgItemText(hwndDlg, IDC_NUMBER, number, numLen + 1);
+                //create contact
+                createContact(name, number);
+                MessageBox(hwndDlg, "A contact has been created!", "Success", NULL);
+                GlobalFree((HANDLE)name);
+                GlobalFree((HANDLE)number);
+                EndDialog(hwndDlg, 0);
+            }
+            else MessageBox(hwndDlg, "Please enter values in the fields!", "Error", NULL);
+        }
+            break;
+        case IDCANCEL:
+            EndDialog(hwndDlg, 0);
+            break;
+        }
+    }
+    break;
+    default:
+        return FALSE;
+    }
+    return TRUE;
+}
+
+BOOL CALLBACK DlgSearch(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch(uMsg) {
+    case WM_INITDIALOG: {}
+    break;
+    case WM_CLOSE: {
+        EndDialog(hwndDlg, 0);
+    }
+    break;
+    case WM_COMMAND: {
+        switch(LOWORD(wParam)) {
+        case IDOK: {
+            int nameLen = GetWindowTextLength(GetDlgItem(hwndDlg, IDC_NAME));
+            if (nameLen > 0) {
+                char *name;
+                char output[1000];
+                name = (char*)GlobalAlloc(GPTR, nameLen + 1);
+                GetDlgItemText(hwndDlg, IDC_NAME, name, nameLen + 1);
+                //search
+                searchContact(name, output);
+                MessageBox(hwndDlg, output, "Success", NULL);
+                GlobalFree((HANDLE)name);
+                EndDialog(hwndDlg, 0);
+            }
+            else MessageBox(hwndDlg, "Please enter values in the fields!", "Error", NULL);
+        }
+            break;
+        case IDCANCEL:
+            EndDialog(hwndDlg, 0);
+            break;
+        }
+    }
+    break;
+    default:
+        return FALSE;
+    }
+    return TRUE;
 }
--- resource.h
+++ resource.h
@@ -1,7 +1,13 @@
-#ifndef RESOURCE_H_INCLUDED
-#define RESOURCE_H_INCLUDED
-
+#ifndef IDC_STATIC
 #define IDC_STATIC (-1)
-#define DLG_MAIN 100
+#endif
 
-#endif // RESOURCE_H_INCLUDED
+#define IDD_DIALOG1                             100
+#define IDD_DIALOG2                             102
+#define IDD_DIALOG3                             103
+#define IDOK                                    40000
+#define IDSEARCH                                40000
+#define IDCANCEL                                40001
+#define IDCREATE                                40001
+#define IDC_NAME                                40002
+#define IDC_NUMBER                              40003
